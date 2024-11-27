4570baf9376d139be9c97083705f71228b3e99c2
Kveld. <kveld912@proton.me>
2024-11-27 01:30:51 -0300

    Add files via upload

--- void-linux/debloat-kde-VOID.sh
+++ void-linux/debloat-kde-VOID.sh
@@ -2,23 +2,58 @@
 
 # Lista de paquetes a verificar y desinstalar si están instalados
 packages=(
-accounts-integration
+adwaita-icon-theme
+avahi-libs
+avahi
 bluedevil
+bluez
+bolt
 btrfs-progs
 cryptsetup
+dolphin-plugins
 grub-i386-efi
 ipw2100-firmware
 ipw2200-firmware
+iw
+iwd
+kaccounts-integration
 kaccounts-providers
+kate
+kdenlive
+kdeplasma-addons
+kgamma
+kinfocenter
+kio-extras
+krita
+ksshaskpass
 ksystemstats
+kwallet-pam
 kwrite
+libldac
 linux-firmware-broadcom
 linux-firmware-intel
 linux-firmware-nvidia
 lvm2
+milou
 oxygen
+oxygen-icons
+oxygen-icons5
+oxygen-qt5
+oxygen-qt6
+oxygen-sounds
+plasma-browser-integration
+plasma-discover
+plasma-systemmonitor
 plasma-thunderbolt
+plasma-vault
+plasma-welcome
 plasma-workspace-wallpapers
+print-manager
+sbx
+signon-kwallet-extension
+sound-theme-freedesktop
+vi
+vim
 void-artwork
 xf86-input-synaptics
 xf86-input-vmmouse
@@ -29,13 +64,17 @@ xf86-video-fbdev
 xf86-video-nouveau
 xf86-video-vesa
 zd1211-firmware
+void-repo-multilib
+void-repo-multilib-nonfree
+void-docs-browse
+void-docs
 )
 
 # Recorre la lista de paquetes
 for pkg in "${packages[@]}"; do
-    if xbps-query -l "$pkg" &>/dev/null; then
+    if xbps-query -R "$pkg" &>/dev/null; then
         echo "Desinstalando $pkg..."
-        xbps-remove -RoF"$pkg"
+        sudo xbps-remove -RoF "$pkg"
     else
         echo "$pkg no está instalado, omitiendo..."
     fi
